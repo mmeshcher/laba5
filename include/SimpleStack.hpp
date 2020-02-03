e6388d15f265c5e5aa674c7f51bef194a54cdc51
mmeshcher <keyvrn12012014@icloud.com>
2020-02-03 12:07:50 +0300

    f

--- include/SimpleStack.hpp
+++ include/SimpleStack.hpp
@@ -1,7 +1,7 @@
 // Copyright 2020 <mmeshcher>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
+#ifndef INCLUDE_SIMPLESTACK_HPP_
+#define INCLUDE_SIMPLESTACK_HPP_
 
 #include <utility>
 #include <stdexcept>
@@ -10,73 +10,73 @@
 template <typename T>
 class SimpleStack
 {
-	struct Node {
-		T data;
-		Node* next = nullptr;
-		Node(T& arg) { data = arg; }
-		Node(T&& arg) {	data = std::move(arg); }
-		static Node* create(const T& arg, Node* pointer) {
-			Node* n = new Node(arg);
-			n->next = pointer;
-			return n;
-		}
-		static Node* create(T&& arg, Node* pointer) {
-			Node* n = new Node(std::move(arg));
-			n->next = pointer;
-			return n;
-		}
-		~Node() {
-			if (next != nullptr) {
-				delete next;
-				next = nullptr;
-			}
-		}
-	};
-	SimpleStack(const SimpleStack& a) {}
-	void operator=(const SimpleStack& a) {}
-	Node* top = nullptr;
+    struct Node {
+        T data;
+        Node* next = nullptr;
+        Node(T& arg) { data = arg; }
+        Node(T&& arg) {    data = std::move(arg); }
+        static Node* create(const T& arg, Node* pointer) {
+            Node* n = new Node(arg);
+            n->next = pointer;
+            return n;
+        }
+        static Node* create(T&& arg, Node* pointer) {
+            Node* n = new Node(std::move(arg));
+            n->next = pointer;
+            return n;
+        }
+        ~Node() {
+            if (next != nullptr) {
+                delete next;
+                next = nullptr;
+            }
+        }
+    };
+    SimpleStack(const SimpleStack& a) {}
+    void operator=(const SimpleStack& a) {}
+    Node* top = nullptr;
 
 public:
-	SimpleStack() {}
-	SimpleStack(T&& value) {
-		push(std::move(value));
-	}
-	SimpleStack(const T& value) {
-		push(value);
-	}
-	SimpleStack(SimpleStack&& r) {
-		top = r.top;
-		r.top = nullptr;
-	}
-	void operator=(SimpleStack&& r) {
-		if (this == &r) return;
-		~SimpleStack();
-		top = r.top;
-		r.top = nullptr;
-	}
-	void push(T&& value) { top = Node::create(std::move(value), top);}
-	void push(const T& value) {	top = Node::create(value, top);}
-	void pop() {
-		if (top == nullptr) return;
-		Node* temp = top;
-		top = temp->next;
-		temp->next = nullptr;
-		delete temp;
-	}
-	const T& head() const {	
-		if (top == nullptr) throw std::logic_error{ "The stack is empty!" };
-		return top->data;
-	}
-	~SimpleStack() {
-		if (top != nullptr) {
-			delete top;
-			top = nullptr;
-		}
-	}
-	operator bool() const{
-		return top != nullptr;
-	}
+    SimpleStack() {}
+    SimpleStack(T&& value) {
+        push(std::move(value));
+    }
+    SimpleStack(const T& value) {
+        push(value);
+    }
+    SimpleStack(SimpleStack&& r) {
+        top = r.top;
+        r.top = nullptr;
+    }
+    void operator=(SimpleStack&& r) {
+        if (this == &r) return;
+        ~SimpleStack();
+        top = r.top;
+        r.top = nullptr;
+    }
+    void push(T&& value) { top = Node::create(std::move(value), top);}
+    void push(const T& value) {    top = Node::create(value, top);}
+    void pop() {
+        if (top == nullptr) return;
+        Node* temp = top;
+        top = temp->next;
+        temp->next = nullptr;
+        delete temp;
+    }
+    const T& head() const {
+        if (top == nullptr) throw std::logic_error{ "The stack is empty!" };
+        return top->data;
+    }
+    ~SimpleStack() {
+        if (top != nullptr) {
+            delete top;
+            top = nullptr;
+        }
+    }
+    operator bool() const{
+        return top != nullptr;
+    }
 };
 
 
-#endif // INCLUDE_HEADER_HPP_
+#endif // INCLUDE_SIMPLESTACK_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -7,145 +7,145 @@
 #include <type_traits>
 
 struct for_emplace {
-	int a = 0;
-	std::string s;
-	for_emplace() {}
-	for_emplace(std::string p, int r) {
-		a = r;
-		s = p;
-	}
-	for_emplace(for_emplace&& r) {
-		a = r.a;
-		s = std::move(r.s);
-	}
-	void operator=(for_emplace&& r) {
-		a = r.a;
-		s = std::move(r.s);
-	}
+    int a = 0;
+    std::string s;
+    for_emplace() {}
+    for_emplace(std::string p, int r) {
+        a = r;
+        s = p;
+    }
+    for_emplace(for_emplace&& r) {
+        a = r.a;
+        s = std::move(r.s);
+    }
+    void operator=(for_emplace&& r) {
+        a = r.a;
+        s = std::move(r.s);
+    }
 private:
-	for_emplace(const for_emplace&);
-	void operator=(const for_emplace&);
+    for_emplace(const for_emplace&);
+    void operator=(const for_emplace&);
 };
 
 TEST(Stack1_base, push_pop_head) {
-	SimpleStack<int> s;
-	s.push(1);
-	EXPECT_EQ(s.head(), 1);
-	s.push(3);
-	EXPECT_EQ(s.head(), 3);
-	s.pop();
-	EXPECT_EQ(s.head(), 1);
+    SimpleStack<int> s;
+    s.push(1);
+    EXPECT_EQ(s.head(), 1);
+    s.push(3);
+    EXPECT_EQ(s.head(), 3);
+    s.pop();
+    EXPECT_EQ(s.head(), 1);
 }
 
 TEST(Stack1_base, Constructors) {
-	SimpleStack<int> s(1234);
-	EXPECT_EQ(s.head(), 1234);
-	s.push(std::move(5678));
-	EXPECT_EQ(s.head(), 5678);
-	SimpleStack<int> z(std::move(9876));
-	EXPECT_EQ(z.head(),9876);
+    SimpleStack<int> s(1234);
+    EXPECT_EQ(s.head(), 1234);
+    s.push(std::move(5678));
+    EXPECT_EQ(s.head(), 5678);
+    SimpleStack<int> z(std::move(9876));
+    EXPECT_EQ(z.head(),9876);
 }
 
 TEST(Stack1_advanced, Move) {
-	SimpleStack<int> s(1234);
-	SimpleStack<int> z(std::move(s));
-	EXPECT_EQ(z.head(), 1234);
-	EXPECT_TRUE(!s);
-	s = std::move(z);
-	EXPECT_EQ(s.head(), 1234);
-	EXPECT_TRUE(!z);
+    SimpleStack<int> s(1234);
+    SimpleStack<int> z(std::move(s));
+    EXPECT_EQ(z.head(), 1234);
+    EXPECT_TRUE(!s);
+    s = std::move(z);
+    EXPECT_EQ(s.head(), 1234);
+    EXPECT_TRUE(!z);
 }
 
 TEST(Stack1_advanced, Self) {
-	SimpleStack<int> s(1234);
-	s = std::move(s);
-	EXPECT_EQ(s.head(), 1234);
+    SimpleStack<int> s(1234);
+    s = std::move(s);
+    EXPECT_EQ(s.head(), 1234);
 }
 
 TEST(Stack1_advanced, Except) {
-	bool flag = false;
-	SimpleStack<int> s;
-	try {
-		s.head();
-	}
-	catch (std::exception &e) {
-		EXPECT_EQ(std::string(e.what()), "The stack is empty!");
-		flag = true;
-	}
-	EXPECT_TRUE(flag);
+    bool flag = false;
+    SimpleStack<int> s;
+    try {
+        s.head();
+    }
+    catch (std::exception &e) {
+        EXPECT_EQ(std::string(e.what()), "The stack is empty!");
+        flag = true;
+    }
+    EXPECT_TRUE(flag);
 }
 
 TEST(Stack2_base, push_pop_head) {
-	SimpleStack<int> s;
-	SimpleStack<int> s1;
-	s.push(1234);
-	s1.push(3456);
-	Stack<SimpleStack<int>> r;
-	EXPECT_TRUE(!r);
-	r.push(std::move(s));
-	EXPECT_EQ(r.head().head(), 1234);
-	r.push(std::move(s1));
-	EXPECT_EQ(r.head().head(), 3456);
-	EXPECT_TRUE(!s);
-	s = std::move(r.pop());
-	EXPECT_EQ(r.head().head(), 1234);
-	EXPECT_EQ(s.head(), 3456);
+    SimpleStack<int> s;
+    SimpleStack<int> s1;
+    s.push(1234);
+    s1.push(3456);
+    Stack<SimpleStack<int>> r;
+    EXPECT_TRUE(!r);
+    r.push(std::move(s));
+    EXPECT_EQ(r.head().head(), 1234);
+    r.push(std::move(s1));
+    EXPECT_EQ(r.head().head(), 3456);
+    EXPECT_TRUE(!s);
+    s = std::move(r.pop());
+    EXPECT_EQ(r.head().head(), 1234);
+    EXPECT_EQ(s.head(), 3456);
 }
 
 TEST(Stack2_base, emplace) {
-	Stack<for_emplace> tester;
-	tester.push_emplace("hello",90);
-	EXPECT_EQ(tester.head().a, 90);
-	EXPECT_EQ(tester.head().s, std::string("hello"));
+    Stack<for_emplace> tester;
+    tester.push_emplace("hello", 90);
+    EXPECT_EQ(tester.head().a, 90);
+    EXPECT_EQ(tester.head().s, std::string("hello"));
 }
 
 TEST(Stack2_base, Constructors) {
-	SimpleStack<int> s;
-	SimpleStack<int> s1;
-	s.push(1234);
-	s1.push(3456);
-	Stack<SimpleStack<int>> r(std::move(s));
-	EXPECT_EQ(r.head().head(), 1234);
-	r.push(std::move(s1));
-	EXPECT_EQ(r.head().head(), 3456);
+    SimpleStack<int> s;
+    SimpleStack<int> s1;
+    s.push(1234);
+    s1.push(3456);
+    Stack<SimpleStack<int>> r(std::move(s));
+    EXPECT_EQ(r.head().head(), 1234);
+    r.push(std::move(s1));
+    EXPECT_EQ(r.head().head(), 3456);
 }
 
 TEST(Stack2_advanced, Move) {
-	SimpleStack<int> s;
-	s.push(1234);
-	Stack<SimpleStack<int>> r(std::move(s));
-	Stack<SimpleStack<int>> p(std::move(r));
-	EXPECT_TRUE(!r);
-	EXPECT_EQ(p.head().head(),1234);
-	r = std::move(p);
-	EXPECT_TRUE(!p);
-	EXPECT_EQ(r.head().head(), 1234);
+    SimpleStack<int> s;
+    s.push(1234);
+    Stack<SimpleStack<int>> r(std::move(s));
+    Stack<SimpleStack<int>> p(std::move(r));
+    EXPECT_TRUE(!r);
+    EXPECT_EQ(p.head().head(), 1234);
+    r = std::move(p);
+    EXPECT_TRUE(!p);
+    EXPECT_EQ(r.head().head(), 1234);
 }
 
 TEST(Stack2_advanced, Self) {
-	SimpleStack<int> s;
-	s.push(1234);
-	Stack<SimpleStack<int>> r(std::move(s));
-	r = std::move(r);
-	EXPECT_EQ(r.head().head(), 1234);
+    SimpleStack<int> s;
+    s.push(1234);
+    Stack<SimpleStack<int>> r(std::move(s));
+    r = std::move(r);
+    EXPECT_EQ(r.head().head(), 1234);
 }
 
 TEST(Stack2_advanced, Except) {
-	bool flag = false;
-	Stack<Stack<int>> r;
-	try {
-		r.head();
-	}
-	catch (std::exception & e) {
-		EXPECT_EQ(std::string(e.what()), "The stack is empty!");
-		flag = true;
-	}
-	EXPECT_TRUE(flag);
+    bool flag = false;
+    Stack<Stack<int>> r;
+    try {
+        r.head();
+    }
+    catch (std::exception & e) {
+        EXPECT_EQ(std::string(e.what()), "The stack is empty!");
+        flag = true;
+    }
+    EXPECT_TRUE(flag);
 }
 
 TEST(Stack_1_2, thatThing) {
-	EXPECT_TRUE(std::is_move_constructible<SimpleStack<int>>());
-	EXPECT_TRUE(std::is_move_constructible<Stack<SimpleStack<int>>>());
-	EXPECT_TRUE(std::is_move_assignable<SimpleStack<int>>());
-	EXPECT_TRUE(std::is_move_assignable<Stack<SimpleStack<int>>>());
+    EXPECT_TRUE(std::is_move_constructible<SimpleStack<int>>());
+    EXPECT_TRUE(std::is_move_constructible<Stack<SimpleStack<int>>>());
+    EXPECT_TRUE(std::is_move_assignable<SimpleStack<int>>());
+    EXPECT_TRUE(std::is_move_assignable<Stack<SimpleStack<int>>>());
 }
